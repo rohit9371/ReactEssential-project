45af0b52e92536a38e1b3290e42f144bbad3c197
rohit9371 <151610530+rohit9371@users.noreply.github.com>
2024-08-03 10:35:57 +0530

    App.jsx

---
+++ src/components/app.jsx
@@ -0,0 +1,21 @@
+import Header from "./header/header.jsx";
+import CoreConcepts from "./CoreConcepts.jsx";
+import Examples from "./Exmaples.jsx";
+
+
+
+function App() {
+  
+  return (
+    <div>
+      <Header />
+      <main>
+        <CoreConcepts />
+        <Examples />
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
